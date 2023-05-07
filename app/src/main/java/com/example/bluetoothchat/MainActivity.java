2ac98f3c7aac3527761ca24c9f3b8899b8b0c2cf
Bash16 <glowingmemory@gmail.com>
2023-05-07 19:18:40 +0500

    first commit

---
+++ app/src/main/java/com/example/bluetoothchat/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.bluetoothchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void signUp(View view) {
+        Log.i("Info", "SignUp Button pressed\n");
+
+        Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+      //  intent.putExtra("message", "Hello, Second Activity!");
+        startActivity(intent);
+    }
+
+   public void login(View view) {
+        Log.i("Info", "Login Button pressed\n");
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+      //  intent.putExtra("message", "Hello, Second Activity!");
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.signUpButton);
+
+    }
+}
